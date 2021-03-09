7ea61f508619048c08ba4a7b46f26bdd84e2ff2d
Kenneth Jones <kennethjones1991@outlook.com>
2021-03-09 03:37:45 -0700

    Removed unneeded comments

--- DoggyData/Doggy.swift
+++ DoggyData/Doggy.swift
@@ -14,35 +14,4 @@ struct Doggy: Codable {
     let lifeExpectancy: String
     let description: String
     let image: String
-    
-//    enum DogKeys: String, CodingKey {
-//        case id
-//        case name
-//        case temperament
-//        case lifeExpectancy = "life-expectancy"
-//        case description
-//        case image
-//    }
-//
-//    init(from decoder: Decoder) throws {
-//        let container = try decoder.container(keyedBy: DogKeys.self)
-//
-//        id = try container.decode(Int.self, forKey: .id)
-//
-//        name = try container.decode(String.self, forKey: .name)
-//
-//        var decodedTemperaments: [String] = []
-//        var temperamentContainer = try container.nestedUnkeyedContainer(forKey: .temperament)
-//        while !temperamentContainer.isAtEnd {
-//            let temperament = try temperamentContainer.decode(String.self)
-//            decodedTemperaments.append(temperament)
-//        }
-//        temperament = decodedTemperaments
-//
-//        lifeExpectancy = try container.decode(String.self, forKey: .lifeExpectancy)
-//
-//        description = try container.decode(String.self, forKey: .description)
-//
-//        image = try container.decode(String.self, forKey: .image)
-//    }
 }
